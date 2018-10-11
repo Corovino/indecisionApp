16dca31103263f099e279557eb996873de1ddcf4
Hebert Romero <hjr@iqthink.com>
2018-10-10 22:09:30 -0500

    avances indecision app

--- src/Components/AddOption.js
+++ src/Components/AddOption.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
+
 class AddOption extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handledAddOption = this.handledAddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
 
     handledAddOption(e) {
         e.preventDefault();
         const options = e.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(options);
 
-        if (options) {
-            console.log(options);
-        }
-
+        this.setSate(() => {
+            return {
+                error: error
+            }
+        });        
     }
 
     render() {
         return (
             <div>
+                {this.state.error && <p>{this.state.error}</p> }
                 <form onSubmit={this.handledAddOption}>
                     <input type="text" name="option" />
                     <button>Add Option</button>
--- src/Components/IndecisionApp.js
+++ src/Components/IndecisionApp.js
@@ -9,9 +9,10 @@ class IndecisionApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            options: ['one', 'two', 'three', 'four']
+            options: []
         }
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
         this.handlePick = this.handlePick.bind(this);
     }
 
@@ -27,6 +28,22 @@ class IndecisionApp extends Component {
         alert(option);
     }
     
+    handleAddOption(option){
+
+        if(!option){
+            return 'Enter a valid value to add item';
+        }else if(this.state.options.indexOf(option) > -1) {
+            return 'this option already exist';    
+        }
+
+
+        this.setState( (prevState) => {
+             return {
+                 options : prevState.options.concat([option])
+             }   
+        })
+    }
+
     render() {
         const title = 'Indecision app';
         const subtitle = 'Put your life n the hands of a computer';
@@ -39,8 +56,10 @@ class IndecisionApp extends Component {
                    handlePick={this.handlePick}/>
                 <Options 
                     options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions} />
-                <AddOption />
+                    handleDeleteOption={this.handleDeleteOption} />
+                <AddOption 
+                    handleAddOption={this.handleAddOption}   
+                />
             </div>
         );
     }
